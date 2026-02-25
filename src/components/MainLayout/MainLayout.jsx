582168eb9f210fcce767d93de24d8ff1195279d6
anndudek <annadudek19920509@gmail.com>
2026-02-25 13:26:40 +0200

    MainLayot, routing, global css

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import {} from "react";
+import cls from "./MainLayout.module.css";
+import { Outlet } from "react-router-dom";
+
+export const MainLayout = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className={cls.mainLayout}>
+      <header>header</header>
+      <div className={cls.mainWrapper}>
+        <main className={cls.main}>main</main>
+        <Outlet />
+        <footer className={cls.footer}>
+          React Question Card App | {currentYear} <br />
+          by Ann Dudek
+        </footer>
+      </div>
+    </div>
+  );
+};
