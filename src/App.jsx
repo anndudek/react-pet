b096c388b94440e7a8690dc92709037e7b6e1c3e
anndudek <annadudek19920509@gmail.com>
2026-03-04 12:06:30 +0200

    Not found page, question card, dynamic route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout";
 import { HomePage } from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   //return <MainLayout />;
@@ -11,6 +12,8 @@ function App() {
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/forbidden" element={<div>forbidden</div>}></Route>
           <Route path="/addquestion" element={<div>addQuestion</div>}></Route>
+          <Route path="/question/:id" element={<div>Question Page</div>}></Route>
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
