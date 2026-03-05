2693b76dfd0ae3a940a0548b051d838875a260ff
anndudek <annadudek19920509@gmail.com>
2026-03-05 16:01:21 +0200

    question card list

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import cls from "./HomePage.module.css";
-import { QuestionCard } from "../../components/QuestionCard";
 import { API_URL } from "../../constants";
+import { QuestionCardList } from "../../components/QuestionCardList";
 
 export const HomePage = () => {
   const [questions, setQuestions] = useState([]);
@@ -23,9 +23,7 @@ export const HomePage = () => {
 
   return (
     <>
-      {questions.map((card, index) => {
-        return <QuestionCard card={card} key={index} />;
-      })}
+      <QuestionCardList cards={questions} />
     </>
   );
 };
