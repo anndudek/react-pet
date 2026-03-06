e0c9eb8d786fc760c1144b33366c427e128fa976
anndudek <annadudek19920509@gmail.com>
2026-03-06 13:46:37 +0200

    input, memo card list

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,11 +3,12 @@ import cls from "./HomePage.module.css";
 import { API_URL } from "../../constants";
 import { QuestionCardList } from "../../components/QuestionCardList";
 import { Loader } from "../../components/Loader";
-import { delayFn } from "../../helpers/delayFn";
 import { useFetch } from "../../hooks/useFetch";
 
 export const HomePage = () => {
   const [questions, setQuestions] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
   const [getQuestions, isLoading, error] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`);
     const questions = await response.json();
@@ -18,8 +19,13 @@ export const HomePage = () => {
     getQuestions("react");
   }, []);
 
+  const onSearchChangeHandler = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <>
+      <input type="text" value={searchValue} onChange={onSearchChangeHandler} />
       {isLoading && <Loader />}
       {error && <p>{error}</p>}
       <QuestionCardList cards={questions} />
