7b445ea6d4d97a662d0ca3751332a4b9f7e7d391
anndudek <annadudek19920509@gmail.com>
2026-04-08 13:58:48 +0300

    count select logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,10 +10,11 @@ import { Button } from "../../components/Button";
 const DEFAULT_PER_PAGE = 10;
 
 export const HomePage = () => {
-  const [searchParams, setSearchParams] = useState(`?_page=2&_per_page=${DEFAULT_PER_PAGE}`);
+  const [searchParams, setSearchParams] = useState(`?_page=1&_per_page=${DEFAULT_PER_PAGE}`);
   const [questions, setQuestions] = useState({});
   const [searchValue, setSearchValue] = useState("");
   const [sortSelectValue, setSortSelectValue] = useState("");
+  const [countSelectValue, setCountSelectValue] = useState("");
 
   const controlContainersRef = useRef();
 
@@ -55,14 +56,19 @@ export const HomePage = () => {
   const onSortSelectChangeHandler = (e) => {
     setSortSelectValue(e.target.value);
 
-    setSearchParams(`?_page=2&_per_page=${DEFAULT_PER_PAGE}&${e.target.value}`);
+    setSearchParams(`?_page=1&_per_page=${countSelectValue}&${e.target.value}`);
   };
 
   const paginationHandler = (e) => {
     if (e.target.tagName === "BUTTON") {
-      setSearchParams(`?_page=${e.target.textContent}&_per_page=${DEFAULT_PER_PAGE}&${sortSelectValue}`);
+      setSearchParams(`?_page=${e.target.textContent}&_per_page=${countSelectValue}&${sortSelectValue}`);
       controlContainersRef.current.scrollIntoView({ behavior: "smooth" });
     }
+  };
+
+  const onCountSelectChangeHandler = (e) => {
+    setCountSelectValue(e.target.value);
+    setSearchParams(`?_page=1&_per_page=${e.target.value}&${sortSelectValue}`);
   };
 
   return (
@@ -77,6 +83,16 @@ export const HomePage = () => {
           <option value="_sort=completed">completed по возрастанию</option>
           <option value="_sort=-completed">completed по убыванию</option>
         </select>
+        <select value={countSelectValue} onChange={onCountSelectChangeHandler} className={cls.select}>
+          <option disabled>count</option>
+          <hr />
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+          <option value="40">40</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </select>
       </div>
 
       {isLoading && <Loader />}
@@ -87,15 +103,17 @@ export const HomePage = () => {
       {cards.length === 0 ? (
         <p className={cls.noCardsInfo}>No cards...</p>
       ) : (
-        <div className={cls.paginationContainer} onClick={paginationHandler}>
-          {pagination.map((value) => {
-            return (
-              <Button key={value} isActive={value === getActivePageNumber()}>
-                {value}
-              </Button>
-            );
-          })}
-        </div>
+        pagination.length > 1 && (
+          <div className={cls.paginationContainer} onClick={paginationHandler}>
+            {pagination.map((value) => {
+              return (
+                <Button key={value} isActive={value === getActivePageNumber()}>
+                  {value}
+                </Button>
+              );
+            })}
+          </div>
+        )
       )}
     </>
   );
